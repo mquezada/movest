f845d68a7e8f3197d2f85f613a62b797aca7b405
Mauricio Q <waxkun@gmail.com>
2012-01-23 02:08:38 -0300

    static files

--- movest/timeline/views.py
+++ movest/timeline/views.py
@@ -1,8 +1,7 @@
 # Create your views here.
 
-
-
 from django.http import HttpResponse, Http404
+from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 from timeline.models import Recurso, Fecha
 
@@ -14,7 +13,7 @@ def index(request):
     for fecha in fechas_list:
         res_list.append(Recurso.objects.filter(fecha_suceso = fecha.id))
     res_list.reverse()  
-    return render_to_response('timeline/index.html', {'res_list': res_list, 'fechas_list' : fechas_list})
+    return render_to_response('timeline/index.html', {'res_list': res_list, 'fechas_list' : fechas_list}, context_instance=RequestContext(request))
 
 def resource(request, res_id):
     r = get_object_or_404(Recurso, id=res_id)
