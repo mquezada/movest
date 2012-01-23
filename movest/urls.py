059bef5f45869a3c86c068eec342a5a4306978d5
Mauricio Q <waxkun@gmail.com>
2012-01-23 19:11:38 -0300

    se mueven todos los static globales a la carpeta media en movest/

--- movest/urls.py
+++ movest/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-
+import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     url(r'^$', 'movest.views.index'),
     url(r'timeline/', include('timeline.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
 
